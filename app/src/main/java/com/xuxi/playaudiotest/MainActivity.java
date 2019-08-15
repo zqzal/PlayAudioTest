9dcda86575703e25ba3440c39a6942c93af03ffd
zqzal <936952020@qq.com>
2019-08-15 17:57:17 +0800

    music.mp3 play

--- app/src/main/java/com/xuxi/playaudiotest/MainActivity.java
+++ app/src/main/java/com/xuxi/playaudiotest/MainActivity.java
@@ -1,13 +1,101 @@
 package com.xuxi.playaudiotest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button play = findViewById(R.id.play);
+        Button pauses = findViewById(R.id.pause);
+        Button stop = findViewById(R.id.stop);
+        play.setOnClickListener(this);
+        pauses.setOnClickListener(this);
+        stop.setOnClickListener(this);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }else {
+            initMediaPlayer(); //初始化MediaPlayer
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    initMediaPlayer();
+                }else {
+                    Toast.makeText(this,"无法播放音频",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void initMediaPlayer() {
+        try{
+            File file = new File(Environment.getExternalStorageDirectory(),"music1.mp3");
+            mediaPlayer.setDataSource(file.getPath()); //指定音频文件的路径
+            mediaPlayer.prepare();//让MediaPlayer进入准备状态
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.play:
+                if (!mediaPlayer.isPlaying()){
+                    mediaPlayer.start(); //开始播放
+                }
+                break;
+            case R.id.pause:
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.pause(); //暂停播放
+                }
+                break;
+            case R.id.stop:
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.reset(); //停止播放
+                    initMediaPlayer();
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
     }
 }
